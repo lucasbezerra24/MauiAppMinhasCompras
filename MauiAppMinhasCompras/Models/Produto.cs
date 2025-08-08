8b213db8c693dc2b5b978166ad4bb8e2a7a26e0b
lucasbezerra24 <alquimialucas21@gmail.com>
2025-08-08 14:53:22 -0300

    ADD MODEL E TROCA DA TELA INICIAL

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MauiAppMinhasCompras.Models
+﻿namespace MauiAppMinhasCompras.Models
 {
-    internal class Produto
+    public class Produto
     {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
     }
 }
